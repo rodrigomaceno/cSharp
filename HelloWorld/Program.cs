7bdceb34b6d20ba96c4119e276ba9b1bf284366c
rodrigomaceno <maceno@gmx.com>
2022-07-01 17:54:43 -0600

    created Hello World using C#

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+// Console.WriteLine("Hello, World!");
+
+
+using System;
+
+// Hello World! program
+namespace HelloWorldApp
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
